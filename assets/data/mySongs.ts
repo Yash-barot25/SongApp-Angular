40a5fc359ed55e203c27fe0c7f3403a6a3db9086
yash2506 <52270905+yash2506@users.noreply.github.com>
2019-07-30 22:06:55 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,9 @@
+import {SongInfo} from '../../app/SongInfo';
+export const MYSONGS: SongInfo[] = 
+[
+    {SongName: 'Despacito',Artist: 'Luis Founsi',Genre: 'Zazz',YearReleased: '2018',image:'Image1.png'},
+    {SongName: 'Shape Of you',Artist: 'Ed sheeren',Genre: 'Pop',YearReleased: '2017',image:'Image2.png'},
+    {SongName: 'Attention',Artist: 'Charlie Puth',Genre: 'funk',YearReleased: '2018',image:'Image3.png'},
+    {SongName: 'Bekhyali',Artist: 'Arijit Singh',Genre: 'Alternative',YearReleased: '2019',image:'Image4.png'}
+
+]
